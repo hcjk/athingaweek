b9af8c46a3d26a2b19683bf94f394e8f146c7bef
Gavin Dinubilo <gavincodes@gmail.com>
2016-02-26 19:35:04 -0800

    remove node_modules, switch to react.js server and client

--- index.js
+++ index.js
@@ -3,17 +3,33 @@ var fs = require("fs");
 var Path = require('path');
 var Hapi = require('hapi');
 
+// For .env files, please don't load your .env into package control
+require('dotenv').config();
+
+
 var server = new Hapi.Server();
 server.connection({port: process.env.PORT || 3000});
 
-server.views({
-  engines: {
-    jade: require('jade')
-  },
-  relativeTo: __dirname + '/views',
+// Required to setup views
+//  js and css get written in uncompiled/{respective_folder} first, then run `gulp` to compile into public
+//  views are in the views/ folder, all view folders should have a corresponding controller, except for
+//  special folders like shared.
+
+require('babel-core/register')({
+    plugins: ['transform-react-jsx']
 });
 
-var globals = require('./globals')(server);
+server.register(require('inert'), () => {});
+server.register(require('vision'), (err) => {
+  server.views({
+      engines: {
+          jsx: require('hapi-react-views')
+      },
+      compileOptions: {}, // optional
+      relativeTo: __dirname,
+      path: 'views'
+  });
+});
 
 
 // Configure routes
