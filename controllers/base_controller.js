7982b993bea3b93517cb2127ad9b5ae74367cc99
Gavin Dinubilo <gavincodes@gmail.com>
2016-03-05 17:42:20 -0800

    sort projects in reverse created order

--- controllers/base_controller.js
+++ controllers/base_controller.js
@@ -3,7 +3,7 @@ const Project = m.Project;
 
 module.exports = {
   index: function(req, res) {
-    Project.find().populate('user').exec(function(err, projects) {
+    Project.find().populate('user').sort('-created_at').exec(function(err, projects) {
       res.view('base/index', {projects: projects});  
     });
   }
